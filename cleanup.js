1c0b125e0fc7a3adf6ff12eb266924e1ce208d3f
Paul Bromwell <paul@itechpros.com>
2023-07-10 13:54:36 -0400

    Initial commit of Servoy WAR Builder GitHub Action

---
+++ cleanup.js
@@ -0,0 +1,28 @@
+/* This file contains teardown logic for the WAR build */
+
+const core = require("@actions/core");
+const childProcess = require("child_process");
+
+try {
+    const servoyVersion = core.getInput("servoy-version");
+
+    // Delete the local WAR builder image, if we have it.
+    const inspectImageProcess = childProcess.spawnSync(
+        'docker',
+        ['image', 'inspect', `ghcr.io/itechpros/servoy_buider:${servoyVersion}`]
+    );
+    if (inspectImageProcess.status === 0) {
+        // Image exists locally, so delete it.
+        core.info(`Deleting local WAR builder for Servoy version: ${servoyVersion}`);
+
+        const removeImageProcess = childProcess.spawnSync(
+            'docker',
+            ['rmi', `ghcr.io/itechpros/servoy_builder:${servoyVersion}`]
+        );
+        if (removeImageProcess.status !== 0)
+            core.error(`Failed to remove local WAR builder for Servoy version: ${servoyVersion}`);
+    }
+} catch (e) {
+    // Don't fail the build if we're unable to clean up. Just log it.
+    console.error(e.message);
+}
---
+++ index.js
@@ -0,0 +1,130 @@
+/* Main application logic */
+
+const core = require("@actions/core");
+const childProcess = require("child_process");
+
+function inputExists(inputName) {
+    let envVariableName = `INPUT_${inputName.replace(/ /g, '_').toUpperCase()}`;
+    return process.env.hasOwnProperty(envVariableName);
+}
+
+try {
+    // Required properties
+    const servoyVersion = core.getInput("servoy-version"),
+          apiKey = core.getInput("api-key"),
+          solutionName = core.getInput("solution-name"),
+          defaultAdminUser = core.getInput("default-admin-user"),
+          defaultAdminPassword = core.getInput("default-admin-password");
+    
+    let commandArguments = [
+        "run", "--rm",
+        "-v", `${process.env.GITHUB_WORKSPACE}:/servoy_code`,
+        `ghcr.io/itechpros/servoy_builder:${servoyVersion}`,
+        "-k", apiKey,
+        "-s", solutionName,
+        "-o", "/servoy_code",
+        "-data", "/servoy_code",
+        "-defaultAdminUser", defaultAdminUser,
+        "-defaultAdminPassword", defaultAdminPassword
+    ];
+
+    let stringFields = {
+        "properties-file": "-p",
+        "properties-file-war": "-pfw",
+        "beans": "-b",
+        "exclude-beans": "-excludeBeans",
+        "lafs": "-l",
+        "exclude-lafs": "-excludeLafs",
+        "drivers": "-d",
+        "exclude-drivers": "-excludeDrivers",
+        "plugins": "-pi",
+        "exclude-plugins": "-excludePlugins",
+        "components": "-crefs",
+        "exclude-components": "-excludeComponentPkgs",
+        "services": "-srefs",
+        "exclude-services": "-excludeServicePkgs",
+        "sample-data-row-count": "-sdcount",
+        "allow-data-model-changes": "-allowDataModelChanges",
+        "import-user-policy": "-importUserPolicy",
+        "context-file-name": "-contextFileName",
+        "log4j-configuration-file": "-log4jConfigurationFile",
+        "web-xml-file-name": "-webXmlFileName",
+        "ng2": "-ng2",
+        "war-file-name": "-warFileName",
+        "additional-solutions": "-nas"
+    },
+        multiValueStringFields = [
+            "beans",
+            "exclude-beans",
+            "lafs",
+            "exclude-lafs",
+            "drivers",
+            "exclude-drivers",
+            "plugins",
+            "exclude-plugins",
+            "components",
+            "exclude-components",
+            "services",
+            "exclude-services"
+        ],
+        booleanFields = {
+            "ignore-build-errors": "-ie",
+            "skip-build": "-sb",
+            "dbi": "-dbi",
+            "export-metadata": "-md",
+            "check-metadata": "-checkmd",
+            "sample-data": "-sd",
+            "i18n": "-i18n",
+            "users": "-users",
+            "tables": "-tables",
+            "overwrite-groups": "-overwriteGroups",
+            "allow-sql-keywords": "-allowSQLKeywords",
+            "stop-on-data-model-changes": "-stopOnDataModelChanges",
+            "skip-database-views-update": "-skipDatabaseViewsUpdate",
+            "override-sequence-types": "-overrideSequenceTypes",
+            "override-default-values": "-overrideDefaultValues",
+            "insert-new-i18n-keys-only": "-insertNewI18NKeysOnly",
+            "add-users-to-admin-group": "-addUsersToAdminGroup",
+            "update-sequences": "-updateSequences",
+            "upgrade-repository": "-upgradeRepository",
+            "use-as-real-admin-user": "-useAsRealAdminUser",
+            "do-not-overwrite-db-server-properties": "-doNotOverwriteDBServerProperties",
+            "overwrite-all-properties": "-overwriteAllProperties",
+            "ng1": "-ng1"
+        };
+    Object.keys(stringFields).forEach((stringField) => {
+        if (!inputExists(stringField)) return;
+
+        commandArguments.push(stringFields[stringField]);
+        
+        let stringFieldValue = core.getInput(stringField),
+            stringFieldValues;
+
+        if (~multiValueStringFields.indexOf(stringField) || (multiValueStringFields === "allow-data-model-changes" && stringFieldValue !== "")) {
+            stringFieldValues = stringFieldValue.split(" ");
+            commandArguments = commandArguments.concat(stringFieldValues);
+        } else {
+            commandArguments.push(stringFieldValue);
+        }
+    });
+    Object.keys(booleanFields).forEach((booleanField) => {
+        if (!inputExists(booleanField)) return;
+
+        let booleanFieldValue = core.getBooleanInput(booleanField);
+        if (!booleanFieldValue) return;
+
+        commandArguments.push(booleanFields[booleanField]);
+    });
+
+    // Our command is now ready. Let 'er rip.
+    const dockerRunProcess = childProcess.spawnSync(
+        "docker", commandArguments,
+        { stdio: "inherit" }
+    );
+    if (dockerRunProcess.status !== 0) {
+        core.setFailed("WAR build failed. Please check the logs for more details.");
+        process.exit();
+    }
+} catch (e) {
+    core.setFailed(e.message);
+}
