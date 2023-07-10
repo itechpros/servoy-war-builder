025ac5f7ac26c208e2f846393f9fa2c9b3288ef1
Paul Bromwell <paul@itechpros.com>
2023-07-10 14:29:27 -0400

    Since we need access to Docker, moving the setup logic into the main file.

--- index.js
+++ index.js
@@ -9,9 +9,45 @@ function inputExists(inputName) {
 }
 
 try {
+    // Check to make sure the requested Servoy version exists in our GitHub Container Registry
+    const servoyVersion = core.getInput("servoy-version");
+    core.info(`Checking for existence of WAR builder for Servoy version: ${servoyVersion}`);
+
+    // Make sure the provided Servoy version number matches the version format (prevent command injection)
+    let servoyVersionFormat = /^\d{4}\.\d{2}(\.\d+)?\.\d{4}$/;
+    if (!servoyVersionFormat.test(servoyVersion)) {
+        core.setFailed(`Invalid Servoy version: ${servoyVersion}`);
+        process.exit();
+    }
+
+    const inspectManifestProcess = childProcess.spawnSync(
+        'docker',
+        ['manifest', 'inspect', `ghcr.io/itechpros/servoy_builder:${servoyVersion}`],
+        { encoding: 'utf-8' }
+    );
+    if (~[null, 1].indexOf(inspectManifestProcess.status)) {
+        // Manifest inspect failed (we don't have that version), so let's output what the command output was and set the failure.
+        core.info(`Docker return code: ${inspectManifestProcess.status}`);
+        core.info(`Docker stdout: ${inspectManifestProcess.stdout}`);
+        core.info(`Docker stderr: ${inspectManifestProcess.stderr}`);
+        core.setFailed(`Servoy version not found: ${servoyVersion}`);
+        process.exit();
+    }
+
+    // Pull down the Docker image
+    core.info(`Downloading WAR builder for Servoy version: ${servoyVersion}`);
+    const pullProcess = childProcess.spawnSync(
+        'docker',
+        ['pull', `ghcr.io/itechpros/servoy_builder:${servoyVersion}`],
+        { stdio: 'inherit' }
+    );
+    if (pullProcess.status === null || pullProcess.status !== 0) {
+        core.setFailed(`Download of WAR builder failed for Servoy version: ${servoyVersion}`);
+        process.exit();
+    }
+
     // Required properties
-    const servoyVersion = core.getInput("servoy-version"),
-          apiKey = core.getInput("api-key"),
+    const apiKey = core.getInput("api-key"),
           solutionName = core.getInput("solution-name"),
           defaultAdminUser = core.getInput("default-admin-user"),
           defaultAdminPassword = core.getInput("default-admin-password");
